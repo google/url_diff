c24b267adc4d0db8ef36234f714af7ecdfb07bfd
Paul Duke <mountainmanduke@gmail.com>
2013-12-08 11:24:17 -0500

    make right URL (second positional arg) optional. now url_diff can be used to enumerate a single URL which I think is more readable.

--- url_diff.py
+++ url_diff.py
@@ -240,7 +240,7 @@ def main():
   arg_parser.add_argument('--names', '-n', default=False, required=False,
       help='only diff URL parameter names.', action='store_true', dest='names_only')
   arg_parser.add_argument('left_url', type=str, help='URL to diff against.  Logically handled as the left argurmnt of diff.', metavar='<left URL>')
-  arg_parser.add_argument('right_url', type=str, help='URL to diff against.  Logically handled as the right argurmnt of diff.', metavar='<right URL>')
+  arg_parser.add_argument('right_url', type=str, help='URL to diff against.  Logically handled as the right argurmnt of diff.', metavar='<right URL>', nargs='?', default='')
   arg_parser.add_argument('--quiet', '-q', action='store_true', help='suppress output and return non-zero if URLs differ.',
                           default=False, required=False)
 
